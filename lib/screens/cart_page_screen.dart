8d36d408177121302dbfd42e1d15141bf9d62f5a
Kwibuka Confiance <swiftmotion2020@gmail.com>
2022-04-19 18:47:54 +0200

    adding input type for payment method

--- lib/screens/cart_page_screen.dart
+++ lib/screens/cart_page_screen.dart
@@ -147,6 +147,7 @@ class _CartScreenState extends State<CartScreen> {
                                 topLeft: Radius.circular(25),
                                 topRight: Radius.circular(25))),
                         child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Padding(
                               padding:
@@ -177,16 +178,19 @@ class _CartScreenState extends State<CartScreen> {
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Container(
+                                  margin: const EdgeInsets.only(left: 10),
                                   width: 60,
                                   child: Image.asset(
                                       'assets/paymentStuff/momo.png'),
                                 ),
                                 Container(
+                                  margin: const EdgeInsets.only(left: 10),
                                   width: 60,
                                   child: Image.asset(
                                       'assets/paymentStuff/visa.jpg'),
                                 ),
                                 Container(
+                                  margin: const EdgeInsets.only(left: 10),
                                   width: 60,
                                   child: Image.asset(
                                       'assets/paymentStuff/PayPal.png'),
@@ -198,7 +202,86 @@ class _CartScreenState extends State<CartScreen> {
                                       'assets/paymentStuff/MasterCard.png'),
                                 )
                               ],
-                            )
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 10.0, left: 10),
+                              child: Text(
+                                "Phone Number",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(305)),
+                                        labelText: 'Enter phone number',
+                                        suffixIcon: const Icon(Icons.phone),
+                                        labelStyle:
+                                            const TextStyle(fontSize: 12),
+                                        isDense: true,
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                // vertical: 4,
+                                                horizontal: 20)),
+                                    textInputAction: TextInputAction.done,
+                                    validator: (value) {
+                                      if (value!.isEmpty &&
+                                          !value.contains('@') &&
+                                          !value.contains('.')) {
+                                        return 'Fill valid email';
+                                      }
+
+                                      return null;
+                                    },
+                                    // onSaved: (value) {
+                                    //   _userInformation = UserInformations(
+                                    //     firstName: _userInformation.firstName,
+                                    //     lastName: _userInformation.lastName,
+                                    //     email: value.toString(),
+                                    //     phoneNumber:
+                                    //         _userInformation.phoneNumber,
+                                    //     password: _userInformation.password,
+                                    //   );
+                                    // },
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.only(top: 20),
+                                    width: double.infinity,
+                                    child: Container(
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          next();
+                                        },
+                                        child: const Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 18),
+                                          child: Text(
+                                            "Confirm Payment",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: 'Poppins',
+                                                fontSize: 18),
+                                          ),
+                                        ),
+                                        style: ElevatedButton.styleFrom(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(30)),
+                                            primary: Theme.of(context)
+                                                .primaryColorDark),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ],
                         ),
                       )
